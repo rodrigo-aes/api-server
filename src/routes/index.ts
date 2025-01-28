9f2e090f8f66dd2938c724d46bd13530ead95a23
RodrigoAES <rodrigoaes.20@hotmail.com>
2025-01-28 00:23:51 -0300

    Removing local files from repository

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,11 @@ import Router from "./Router"
 import V1Router from '@/routes/API/V1'
 
 const applicactionRouter = new Router()
+
+applicactionRouter.get('/ping', (req, res) => {
+    res.json({ pong: true })
+})
+
 applicactionRouter.use(V1Router.name, V1Router)
 
 export default applicactionRouter
